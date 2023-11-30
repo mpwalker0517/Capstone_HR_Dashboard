5926f262003d4ba333d7528b712783d9643028cb
Morgan Walker <mpwalker517@gmail.com>
2023-11-30 10:38:09 -0500

    names changed

--- call_coach_pull.sql
+++ call_coach_pull.sql
@@ -3,4 +3,5 @@ FROM `mwalkerlcs-morgan_rm12`.`userdefinedfields`
 INNER JOIN `mwalkerlcs-morgan_rm12`.`userdefinedvalues` ON `userdefinedfields`.`UserDefinedFieldID`=`userdefinedvalues`.`UserDefinedFieldID`
 INNER JOIN `mwalkerlcs-morgan_rm12`.`contacts` ON `contacts`.`EntityKeyID`=`userdefinedvalues`.`EntityKeyID`
 INNER JOIN `mwalkerlcs-morgan_rm12`.`leases` ON `contacts`.`EntityKeyID`=`leases`.`CustomerID`
-WHERE `userdefinedfields`.`EntityTypeID`=1 AND `leases`.`MoveOutDate` IS NULL AND `userdefinedfields`.`UserDefinedFieldID`= 10;
+WHERE `userdefinedfields`.`EntityTypeID`=1 AND `leases`.`MoveOutDate` IS NULL 
+AND `userdefinedfields`.`UserDefinedFieldID`=10 OR `userdefinedfields`.`UserDefinedFieldID`=11 OR `userdefinedfields`.`UserDefinedFieldID`=12 OR `userdefinedfields`.`UserDefinedFieldID`=13;
